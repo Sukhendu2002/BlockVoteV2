9e801e995189a8e2a67c46ef72bb4a0408f4530e
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2023-11-30 15:35:28 +0530

    Add FashMesh

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,13 +1,20 @@
+import { FaceMesh } from "@mediapipe/face_mesh";
 import React, { useEffect, useRef, useState } from "react";
+import * as Facemesh from "@mediapipe/face_mesh";
+import * as cam from "@mediapipe/camera_utils";
 import Webcam from "react-webcam";
 
 const videoConstraints = {
   width: 540,
-  facingMode: "environment",
+  facingMode: "user",
 };
 
 const Camera = ({ setFileImg2 }) => {
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const connect = window.drawConnectors;
+  var camera = null;
+
   const [url, setUrl] = useState(null);
   const capturePhoto = React.useCallback(
     async (e) => {
@@ -29,6 +36,83 @@ const Camera = ({ setFileImg2 }) => {
     [webcamRef]
   );
 
+  const onResults = (results) => {
+    // const video = webcamRef.current.video;
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION, {
+          color: "#C0C0C070",
+          lineWidth: 1,
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {
+          color: "#FF3030",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, {
+          color: "#30FF30",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, {
+          color: "#E0E0E0",
+        });
+        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, {
+          color: "#E0E0E0",
+        });
+      }
+    }
+    canvasCtx.restore();
+  };
+  useEffect(() => {
+    const faceMesh = new FaceMesh({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+      },
+    });
+
+    faceMesh.setOptions({
+      maxNumFaces: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    faceMesh.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await faceMesh.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  }, []);
   useEffect(() => {
     // setVoterData({
     //   ...voterData,
@@ -66,8 +150,19 @@ const Camera = ({ setFileImg2 }) => {
             height: "auto",
             border: "1px solid #000",
             borderRadius: "5px",
+            zIndex: "9",
           }}
         />
+        <canvas
+          ref={canvasRef}
+          className="output_canvas"
+          style={{
+            width: "100%",
+            height: "auto",
+            border: "1px solid #000",
+            borderRadius: "5px",
+          }}
+        ></canvas>
         <button
           className="bg-black text-white px-4 py-2 rounded-md"
           onClick={capturePhoto}
